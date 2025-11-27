4a436366de535a7a3b8f7cfdc2a37f5881ebb36e
Rohan Kumar Mallah <rohanmallah0@gmail.com>
2025-11-27 11:08:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,6 @@ import os
 # ===========================
 model = pickle.load(open(r"C:\Users\rohan\OneDrive\Desktop\Project\model.pkl", "rb"))
 scaler = pickle.load(open(r"C:\Users\rohan\OneDrive\Desktop\Project\scaler.pkl", "rb"))
-model = pickle.load(open(r"C:\Users\rohan\OneDrive\Desktop\Project\model.pkl", "rb"))
-model = pickle.load(open("model.pkl", "rb"))
-
-
 
 # ===========================
 # PAGE SETUP + CUSTOM CSS
@@ -165,5 +161,3 @@ if file:
     else:
         st.error("CSV must contain 'Predicted Salary' column!")
 st.markdown("</div>", unsafe_allow_html=True)
-
-
